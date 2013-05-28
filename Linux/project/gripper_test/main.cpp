3f82cddb8776d39eb44b1f3d2bf96606996e354b
tician <tician@gmail.com>
2013-05-28 10:12:32 -0400

    Now to start on the AXM compatible version...

--- Linux/project/gripper_test/main.cpp
+++ Linux/project/gripper_test/main.cpp
@@ -33,90 +33,29 @@ int main(void)
 			return 0;
 	}
 	MotionManager::GetInstance()->AddModule((MotionModule*)Gripper::GetRight());
-//	MotionManager::GetInstance()->AddModule((MotionModule*)Gripper::GetLeft());
     LinuxMotionTimer *motion_timer = new LinuxMotionTimer(MotionManager::GetInstance());
     motion_timer->Start();
 
 	std::cout << "My ID is: " << Gripper::GetRight()->GetID() << std::endl;
 	Gripper::GetRight()->Initialize();
 	sleep(5);
-//	std::cout << "My ID is: " << Gripper::GetLeft()->GetID() << std::endl;
-//	Gripper::GetLeft()->Initialize();
-//	sleep(2);
 
 	MotionStatus::m_CurrentJoints.SetEnableBody(false);
 	MotionStatus::m_CurrentJoints.SetEnable(JointData::ID_R_GRIPPER, true);
-//	MotionStatus::m_CurrentJoints.SetEnable(JointData::ID_L_GRIPPER, true);
 	MotionManager::GetInstance()->SetEnable(true);
 	/////////////////////////////////////////////////////////////////////
 
 	Gripper::GetRight()->m_Joint.SetEnableBody(false);
 	Gripper::GetRight()->m_Joint.SetEnable(JointData::ID_R_GRIPPER, true);
-//	Gripper::GetLeft()->m_Joint.SetEnableBody(false);
-//	Gripper::GetLeft()->m_Joint.SetEnable(JointData::ID_L_GRIPPER, true);
 
 	Gripper::GetRight()->m_Joint.SetPGain(JointData::ID_R_GRIPPER, 8);
 
     while(1)
     {
-/*    	std::cout << "Moving to neutral\n";
-        Gripper::GetLeft()->MoveToNeutral(.3);
-        sleep(2);
-        std::cout << "\'Load\': " << Gripper::GetLeft()->GetTorqueNow() << std::endl;
-    	std::cout << "Moving to open\n";
-        Gripper::GetLeft()->MoveToOpen(1.0);
-        sleep(2);
-        std::cout << "\'Load\': " << Gripper::GetLeft()->GetTorqueNow() << std::endl;
-    	std::cout << "Moving to closed\n";
-        Gripper::GetLeft()->MoveToClosed(.01);
-        sleep(2);
-        std::cout << "\'Load\': " << Gripper::GetLeft()->GetTorqueNow() << std::endl;
-*/
-/*
-    	std::cout << "Moving to neutral\n";
-        Gripper::GetRight()->MoveToNeutral(.3);
-        sleep(2);
-        std::cout << "\'Load\': " << Gripper::GetRight()->GetTorqueNow() << std::endl;
-    	std::cout << "Moving to open\n";
-        Gripper::GetRight()->MoveToOpen(1.0);
-        sleep(2);
-        std::cout << "\'Load\': " << Gripper::GetRight()->GetTorqueNow() << std::endl;
-    	std::cout << "Moving to closed\n";
-        Gripper::GetRight()->MoveToClosed(.01);
-        sleep(2);
-        std::cout << "\'Load\': " << Gripper::GetRight()->GetTorqueNow() << std::endl;
-*/
-/*
-        double countChocula = 0.0;
-        while(countChocula<15.0)
-        {
-            Gripper::GetRight()->MoveToAngle(countChocula);
-	        std::cout << "\'Load\': " << Gripper::GetRight()->GetTorqueNow() << std::endl;
-            countChocula += 1.0;
-            usleep(100000);
-        }
-        while(countChocula>-80.0)
-        {
-            Gripper::GetRight()->MoveToAngle(countChocula);
-	        std::cout << "\'Load\': " << Gripper::GetRight()->GetTorqueNow() << std::endl;
-            countChocula -= 1.0;
-            usleep(100000);
-        }
-        while(countChocula<0.0)
-        {
-            Gripper::GetRight()->MoveToAngle(countChocula);
-	        std::cout << "\'Load\': " << Gripper::GetRight()->GetTorqueNow() << std::endl;
-            countChocula += 1.0;
-            usleep(100000);
-        }
-*/
-//		Gripper::GetRight()->MoveToOpen();
-//		sleep(5);
 		Gripper::GetRight()->Squeeze(0.2);
 		sleep(5);
 		Gripper::GetRight()->Spread(0.3);
 		sleep(5);
-
     }
 
     return 0;
